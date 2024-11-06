a52e726db9686b9c43a39705edaed2d41087ac1d
Julian Bliemel <Julian.Bliemel@drheuer.de>
2024-11-06 15:54:18 +0100

    d

---
+++ LinqDatenaufbereitung/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDatenaufbereitung;
+
+internal class Product
+{
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+
+    public string Category { get; set; }
+
+    public override string ToString() => $"{Name} , {Price:C} , {Category}";
+}
---
+++ LinqDatenaufbereitung/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinqDatenaufbereitung;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string [] productstring = File.ReadAllLines( @"C:\Users\ITA5-TN05\Desktop\Produkte.txt" );
+List<Product> products = new();
+
+foreach ( string line in productstring )
+{
+    var p = new Product();
+    string [] fields = line.Split( ';' );
+    p.Name = fields [ 0 ];
+    fields [ 1 ] = fields [ 1 ].Replace( '.' , ',' );
+    p.Price = double.Parse( fields [ 1 ] );
+    p.Category = fields [ 2 ];
+    products.Add( p );
+    Console.WriteLine( p );
+}
+
+var avgpricepercatgeory = products.GroupBy( p => p.Category , p => p.Name );
+
+foreach ( var product in products )
+
+    Console.ReadLine();
